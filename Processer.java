adede5500169e5b5d22d575d44d84a53f673a8e7
shanjunmei <445052471@qq.com>
2016-03-20 23:08:18 +0800

    Added files via upload

---
+++ Processer.java
@@ -0,0 +1,147 @@
+package com.lanhun.codeProcess;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.OutputStreamWriter;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Processer {
+
+	public static void main(String[] args) {
+
+		File current = new File(".");
+		File[] files = current.listFiles();
+		for (File file : files) {
+			if (file.isDirectory()) {
+				dirProcess(file, null);
+			}
+		}
+
+	}
+
+	private static void dirProcess(File file, String root) {
+		if (root == null) {
+			root = file.getAbsolutePath();
+		}
+		System.out.println(root);
+		File[] processFiles = file.listFiles();
+		for (File file2 : processFiles) {
+			if (!file2.isDirectory()) {
+				TextFile textFile = read(file2, root);
+				process(textFile);
+				write(textFile);
+			} else {
+				dirProcess(file2, root);
+			}
+		}
+	}
+
+	private static void write(TextFile textFile) {
+		String fileName = textFile.getFileName();
+		fileName = fileName.replace(textFile.getRoot(), textFile.getRoot()
+				+ ".cs");
+		FileOutputStream fos = null;
+		OutputStreamWriter os = null;
+		try {
+			File file = new File(fileName);
+			if (!file.getParentFile().exists()) {
+				file.getParentFile().mkdirs();
+			}
+			fos = new FileOutputStream(file);
+			os = new OutputStreamWriter(fos, textFile.getEncoding());
+			os.write(textFile.getContent());
+			os.flush();
+			os.close();
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+
+	}
+
+	/**
+	 * 文件处理
+	 * 
+	 * @param textFile
+	 */
+	private static void process(TextFile textFile) {
+		String content = textFile.getContent();
+		// set/get方法移除
+		String patternExp = "public .*[gs]et.*[\\s\\S]*?}";
+		Pattern pattern = Pattern.compile(patternExp);
+		Matcher matcher = pattern.matcher(content);
+		while (matcher.find()) {
+			content = content.replace(matcher.group(), "");
+
+		}
+
+		// 引用导入转换，去重
+		patternExp = "import com.hzins.channel.api.model.info.*";
+		pattern = Pattern.compile(patternExp);
+		matcher = pattern.matcher(content);
+		boolean notFirst = false;
+		while (matcher.find()) {
+			if (!notFirst) {
+				content = content.replace(matcher.group(),
+						"using com.hzins.channel.api.model.info;");
+			} else {
+				content = content.replace(matcher.group(), "");
+			}
+			notFirst = true;
+		}
+
+		patternExp = "com.hzins.channel.api.model.common.*";
+		pattern = Pattern.compile(patternExp);
+		matcher = pattern.matcher(content);
+		notFirst = false;
+		while (matcher.find()) {
+			if (!notFirst) {
+				content = content.replace(matcher.group(),
+						"using com.hzins.channel.api.model.common;");
+			} else {
+				content = content.replace(matcher.group(), "");
+			}
+			notFirst = true;
+		}
+
+		// 私有属性转换为公有属性
+		content = content.replace("private", "public");
+
+		content = content.replace("import java.util.List",
+				"using System.Collections.Generic");
+		content = content.replace("import java.util.Map",
+				"using System.Collections.Generic");
+
+		textFile.setContent(content);
+	}
+
+	/**
+	 * 文件读取
+	 * 
+	 * @param fileName
+	 * @return
+	 */
+	private static TextFile read(File file, String root) {
+		TextFile textFile = new TextFile();
+		textFile.setRoot(root);
+		try {
+			FileReader reader = new FileReader(file);
+			String encoding = reader.getEncoding();
+			textFile.setFileName(file.getAbsolutePath());
+			textFile.setEncoding(encoding);
+			StringBuffer sb = new StringBuffer();
+			int length = 0;
+			char[] buf = new char[1024];
+			while ((length = reader.read(buf)) != -1) {
+				sb.append(buf, 0, length);
+			}
+			textFile.setContent(sb.toString());
+			reader.close();
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+		return textFile;
+	}
+
+}
---
+++ TextFile.java
@@ -0,0 +1,45 @@
+package com.lanhun.codeProcess;
+
+public class TextFile {
+	// 文件编码
+	private String encoding;
+	// 文件类容
+	private String content;
+
+	private String fileName;
+
+	private String root;
+
+	public String getEncoding() {
+		return encoding;
+	}
+
+	public void setEncoding(String encoding) {
+		this.encoding = encoding;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public String getFileName() {
+		return fileName;
+	}
+
+	public void setFileName(String fileName) {
+		this.fileName = fileName;
+	}
+
+	public String getRoot() {
+		return root;
+	}
+
+	public void setRoot(String root) {
+		this.root = root;
+	}
+
+}
